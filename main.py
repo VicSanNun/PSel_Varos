c6a61510933c2bf2fb054a69f260d69dfc2b5e43
VicSanNun <victornunes470@gmail.com>
2024-01-03 18:31:32 -0300

    get stock data

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from scrapping_news import get_news
+from get_stocks import get_stock_data
 
 BASE_URL = "https://braziljournal.com/"
-search = "c%26a"
+#search = "c%26a"
+search = "petr4"
+ticker = "PETR4.SA"
+start_date = "2023-01-01"
 
+stock_data = get_stock_data(ticker, start_date)
 articles = get_news(BASE_URL, search)
 
 for article, _ in zip(articles[:3], range(3)):
@@ -12,3 +17,5 @@ for article, _ in zip(articles[:3], range(3)):
     print(f"Header: {header_text}")
     print(f"Link: {link}")
     print("------")
+
+print(stock_data)
