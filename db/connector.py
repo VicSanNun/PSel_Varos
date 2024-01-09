ea1f4015b38b97585f30ee2cf96ee63a48c86491
Victor Nunes <victornunes470@gmail.com>
2024-01-08 23:58:32 -0300

    Update connector.py

--- db/connector.py
+++ db/connector.py
@@ -1,3 +1,5 @@
 from sqlalchemy import create_engine
 def conn():
+    #Não é recomendado subir suas credenciais no repositório. O ideal é ter um arquivo local (ou o ambiente configurado) 
+    #com as credenciais.
     return create_engine('mysql+mysqlconnector://psel_varos:psel_varos@localhost/psel_varos', echo=True)
